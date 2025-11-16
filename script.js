d4de1ff038cb73e50c7974a18bde76a050fda251
Nilesh043 <23p65a0508@vbithyd.ac.in>
2025-11-16 08:39:45 +0530

    images

--- script.js
+++ script.js
@@ -5,86 +5,98 @@ let team = [
     win: 5,
     primary: "Yellow",
     secondary: "#0054A8",
+    image: "chennai.webp",
   },
-  { 
+  {
     name: "Mumbai Indians",
-    Captain: "Hardik Pandya", 
+    Captain: "Hardik Pandya",
     win: 5,
     primary: "Blue",
-    secondary: "Gold", 
+    secondary: "orange",
+    image: "mumbai.webp",
   },
-  { 
-    name: "Kolkata Knight Riders", 
-    Captain: "Ajinkya Rahane", 
+  {
+    name: "Kolkata Knight Riders",
+    Captain: "Ajinkya Rahane",
     win: 2,
     primary: "purple",
-    secondary: "Gold", 
+    secondary: "Gold",
+    image: "kkr.webp",
   },
-  { 
-    name: "Royal Challengers Bangalore", 
-    Captain: "Rajat Patidar", 
+  {
+    name: "Royal Challengers Bangalore",
+    Captain: "Rajat Patidar",
     win: 1,
     primary: "Red",
-    secondary: "Black", 
+    secondary: "Black",
+    image: "rcb.webp",
   },
-  { 
-    name: "Delhi Capitals", 
-    Captain: "Axar Patel", 
+  {
+    name: "Delhi Capitals",
+    Captain: "Axar Patel",
     win: 1,
     primary: "Blue",
-    secondary: "Red", 
+    secondary: "Red",
+    image: "DC.webp",
   },
-  { 
-    name: "Sunrisers Hyderabad", 
-    Captain: "Pat cummins", 
+  {
+    name: "Sunrisers Hyderabad",
+    Captain: "Pat cummins",
     win: 1,
     primary: "Orange",
-    secondary: "Black", 
+    secondary: "Black",
+    image: "srh.webp",
   },
-  { 
-    name: "Punjab Kings", 
-    Captain: "Shreyas Iyer", 
+  {
+    name: "Punjab Kings",
+    Captain: "Shreyas Iyer",
     win: 0,
     primary: "Red",
     secondary: "#F1E5AC",
+    image: "pbks.webp",
   },
-  { 
-    name: "Rajasthan Royals", 
-    Captain: "Sanju Samson", 
+  {
+    name: "Rajasthan Royals",
+    Captain: "Sanju Samson",
     win: 0,
     primary: "Pink",
-    secondary: "skyblue", 
+    secondary: "skyblue",
+    image: "rr.webp",
   },
-  { 
-    name: "Gujarat Titans", 
-    Captain: "Shubman Gill", 
+  {
+    name: "Gujarat Titans",
+    Captain: "Shubman Gill",
     win: 1,
     primary: "#000035",
     secondary: "#F1E5AC",
+    image: "gt.webp",
   },
-  { 
-    name: "Lucknow Super Giants", 
-    Captain: "Rishab Pant", 
+  {
+    name: "Lucknow Super Giants",
+    Captain: "Rishab Pant",
     win: 0,
     primary: "Blue",
-    secondary: "Silver", 
+    secondary: "Silver",
+    image: "lsg.webp",
   },
 ];
 
+let tname = document.querySelector(".name");
+let captain = document.querySelector(".cap");
+let wins = document.querySelector(".times");
+let btn = document.querySelector("button");
+let ctn = document.querySelector(".ctn");
+let main = document.querySelector("main");
+let img = document.querySelector(".img");
 
-let tname = document.querySelector('.name');
-let captain = document.querySelector('.cap');
-let wins = document.querySelector('.times');
-let btn = document.querySelector('button');
-let ctn = document.querySelector('.ctn');
-let main = document.querySelector('main');
-
-btn.addEventListener('click', function() {
+btn.addEventListener("click", function () {
   let selected = team[Math.floor(Math.random() * team.length)];
   tname.textContent = selected.name;
   captain.textContent = selected.Captain;
   wins.textContent = selected.win;
   ctn.style.backgroundColor = selected.primary;
-  
-  main.style.background = `linear-gradient(45deg, ${selected.primary}, ${selected.secondary})`;
+
+  main.style.background = `linear-gradient(to left, ${selected.primary} 10%, ${selected.secondary}) 90%`;
+  btn.style.background = `linear-gradient(45deg, ${selected.primary}, ${selected.secondary})`;
+  img.style.backgroundImage = `url('./images/${selected.image}')`;
 });
